1f25cff527f12509d9bd4a40e8250e3194eb4d83
Oluwakemi Yusuf <yusufoluwakemi39@gmail.com>
2022-02-07 11:05:32 +0100

    use slot to create the dashboardlayout

---
+++ src/Layouts/DashboardLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen flex">
+    <!--side nav--->
+    <slot name="sidebar"> </slot>
+
+    <!--main-content--->
+    <div class="flex-1 min-w-0 flex flex-col bg-white">
+      <div class="flex-shrink-0 flex-col border-b-2 border-gray-200">
+        <slot name="main-header"></slot>
+      </div>
+      <div class="flex-1 overflow-auto">
+        <slot name="kanban-area"> </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardLayout",
+};
+</script>
+
+<style scoped></style>
